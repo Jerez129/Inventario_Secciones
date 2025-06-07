a7eba5d562567eb9bb82446ed6214d4859acae9f
jerez129 <japf20092004@gmail.com>
2025-06-07 14:30:24 -0400

    version 1.3

--- Capa_Datos/Program.cs
+++ Capa_Datos/Program.cs
@@ -1,2 +1,22 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+﻿using System;
+
+namespace Capa_Datos
+{
+
+    public class Inventarios
+    {
+
+       // string[][] Inventario = new string[][] 
+
+
+    }
+   
+
+
+
+}
+>>>>>>> version 1.2
---
+++ Inventario_Secciones/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Inventario_Secciones
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_Nombre = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // txt_Nombre
+            // 
+            txt_Nombre.AutoSize = true;
+            txt_Nombre.Location = new Point(85, 52);
+            txt_Nombre.Name = "txt_Nombre";
+            txt_Nombre.Size = new Size(64, 20);
+            txt_Nombre.TabIndex = 0;
+            txt_Nombre.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(85, 179);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Cantidad";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(84, 358);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(84, 303);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Seccion";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 91);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(85, 235);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(275, 357);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 29);
+            button1.TabIndex = 8;
+            button1.Text = "Agregar Seccion";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(559, 358);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 29);
+            button2.TabIndex = 9;
+            button2.Text = "Agregar Producto";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(txt_Nombre);
+            Name = "Form2";
+            Text = "Cantidad";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label txt_Nombre;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
